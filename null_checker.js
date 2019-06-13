3b2d3ebae5f46e356cf07cf001da8e58cd598675
Denis774 <51455035+Denis774@users.noreply.github.com>
2019-06-13 21:52:01 +0300

    Add files via upload

---
+++ null_checker.js
@@ -0,0 +1,19 @@
+
+
+for (count=1;count<11;count++)
+
+{
+	let num =prompt ("Input a number");
+/*if the number is less than zero*/	
+		if (num<0)
+			console.log(-1);
+/* in the value is greater than zero*/
+
+else if ( num > 0)
+
+	console.log(1);
+//if the value equals zero*/
+else
+	 console.log(0);
+	}
+
