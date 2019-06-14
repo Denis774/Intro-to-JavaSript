fc6681c39f536932779c61fc72c223ae59fca985
Denis774 <51455035+Denis774@users.noreply.github.com>
2019-06-14 09:15:58 +0300

    Update null_checker.js

--- null_checker.js
+++ null_checker.js
@@ -6,11 +6,11 @@ for (x=1;x<11;x++)
 	Prompt ("Input a number");
 	
 /*if the number is less than zero*/	
-		if (num<0)
+		if (x<0)
 			console.log(-1);
 /* in the value is greater than zero*/
 
-else if ( num > 0)
+else if ( x> 0)
 
 	console.log(1);
 //if the value equals zero*/
